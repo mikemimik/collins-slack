57867a43e96e3e057b6a64906560f776d406689b
Michael Perrotte <mike@mikecorp.ca>
2016-02-11 03:04:47 -0500

    added onConnect() listener

--- libs/Listeners.js
+++ libs/Listeners.js
@@ -4,6 +4,11 @@ class Listeners {
   static onMessage(message) {
     console.log('>>', 'CollinsSlack', 'msg', message);
   }
+
+  static onConnecting() {
+    let args = Array.prototype.splice.apply(arguments);
+    console.log('>>', 'CollinsSlack', 'onConnecting', args);
+  }
 }
 
 module.exports = Listeners;
