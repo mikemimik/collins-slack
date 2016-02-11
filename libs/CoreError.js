95950341c12953d7987af13d679f19c217781c68
Michael Perrotte <mike@mikecorp.ca>
2016-02-11 04:32:50 -0500

    updated CoreError

--- libs/CoreError.js
+++ libs/CoreError.js
@@ -15,14 +15,20 @@ class ExtendableError extends Error {
  * @param {String} type The error type name
  * @param {Object} data The data from the error
  */
-class CoreError extends ExtendableError {
+class SlackError extends ExtendableError {
   constructor(type, data) {
     super('constructor'); // TODO: is this correct?
-    this.data = data;
-
-    this.message = '\'' + this.name + '\' caused an error';
+    this.data = (data) ? data : {};
+    this.type = type;
+    this.message = '\'' + type + '\' error message received';
+    let reason = data.details || data.reason;
+    if (reason) {
+      this.message += ':\n"' + reason + '"\n';
+    } else {
+      this.message += '. ';
+    }
     this.message += 'See \'data\' for details.';
   }
 }
 
-module.exports = CoreError;
+module.exports = SlackError;
