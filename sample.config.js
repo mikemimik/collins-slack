47c0749063b9a0963d4cbb28a67cdcaf520002d4
Michael Perrotte <mike@mikecorp.ca>
2016-02-23 05:01:36 -0500

    added sample config file

---
+++ sample.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Filename: slack.config.js
+ *
+ * Contents:
+ * - token
+ *
+ * Inheritables:
+ * - userAgent
+ * - debug
+ */
+
+
+module.exports = {
+  token: 'xoxb-xxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx',
+  userAgent: 'inherit',
+  debug: 'inherit'
+};
