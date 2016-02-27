e3275511b26b8da47b017a7fbc3466cb00e011f1
Michael Perrotte <mike@mikecorp.ca>
2016-02-27 04:51:14 -0500

    updated Listeners class - added getMethods function

--- libs/Listeners.js
+++ libs/Listeners.js
@@ -1,10 +1,40 @@
 'use strict';
 
+// INFO: service-gear specific modules
 const Service = require('../utils/Service');
 
+// INFO: common modules
+const _ = require('lodash');
+
 class Listeners {
-  static message(context, data) {
-    Service.interpret.bind(context, data);
+  static message(data, context) {
+    console.log('Listeners.message:', 'data:', data, 'context:', context);
+    if (data.subtype) {
+      Service.interpret(data.message, this);
+    } else {
+      Service.interpret(data, this);
+    }
+
+  }
+
+  /**
+   * Function to return static functions of this class
+   *
+   * @retuns {Array} methods List of static functions in this class
+   */
+  static getMethods() {
+
+    // INFO: get all properties of this class (object)
+    let methods = Object.getOwnPropertyNames(this);
+
+    // INFO: filter out ones we don't want
+    methods = _.filter(methods, (prop) => {
+      return prop !== 'name'
+        && prop !== 'length'
+        && prop !== 'prototype'
+        && prop !== 'getMethods';
+    });
+    return methods;
   }
 }
 
