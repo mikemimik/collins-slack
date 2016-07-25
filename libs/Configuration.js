c20b20afe54739e90b7748ec5e24f1238de733e7
afrolionx <mike@mikecorp.ca>
2016-07-25 01:21:05 -0400

    added Configuration Object class

---
+++ libs/Configuration.js
@@ -0,0 +1,24 @@
+'use strict';
+const convict = require('convict');
+const Config = require('../configs');
+
+function Configuration () {
+  this.isConfigured = false;
+  this.options = {};
+  this.path = '';
+  this.files = [];
+
+  // INFO: initialize convict
+  Config.formats.forEach((f) => { convict.addFormat(f); });
+  this.configObj = convict(Config.schema);
+}
+
+Configuration.prototype.configure = function configure () {
+
+};
+
+Configuration.prototype.setPath = function setPath (p) {
+
+};
+
+module.exports = Configuration;
